bcfbfa707d800ec415ae4882703a38047ce2b46b
luiz200 <luiz.fhs2015@gmail.com>
2022-02-17 19:07:19 -0300

    1.3

--- prova/src/calendario.js
+++ prova/src/calendario.js
@@ -10,30 +10,33 @@ function Calendario(){
                 <Card.Body>
                     <Card.Title>Calendário</Card.Title>
                     <br></br>
-                    <Card.Title>2021.1</Card.Title>
-                    <Card.Text>
-                    </Card.Text>
-                    <Card.Text>
-                    <small className="text-muted"></small>
-                    </Card.Text>
-                    <Card.Text>
-                    </Card.Text>
-                    <Card.Text>
-                    <small className="text-muted"></small>
-                    </Card.Text>
-                </Card.Body>
-                <Card.Body>
-                    <Card.Title>2021.2</Card.Title>
-                    <Card.Text>
-                    </Card.Text>
-                    <Card.Text>
-                    <small className="text-muted"></small>
-                    </Card.Text>
-                    <Card.Text>
-                    </Card.Text>
-                    <Card.Text>
-                    <small className="text-muted"></small>
-                    </Card.Text>
+                      <h5 className="card-title">2021.1</h5>
+                      <p className="card-text">14/05/2021 à 21/05/2021</p>
+                      <p className="card-text"><small class="text-muted">Matrícula para o período 2021.1</small></p>
+                      <p className="card-text">07/06/2021 à 08/06/2021</p>
+                      <p className="card-text"><small class="text-muted">Re-matrícula para o período 2021.1</small></p>
+                      <p className="card-text">07/06/2021</p>
+                      <p className="card-text"><small class="text-muted">Início do período letivo 2021.1</small></p>
+                      <p className="card-text">14/06/2021</p>
+                      <p className="card-text"><small class="text-muted">Início do período trancamento de turma 2021.1</small></p>
+                      <p className="card-text">16/07/2021</p>
+                      <p className="card-text"><small class="text-muted">Término do período trancamento de turma 2021.1</small></p>
+                      <p className="card-text">31/10/2021</p>
+                      <p className="card-text"><small class="text-muted">Término do período letivo 2021.1</small></p>
+                    <br></br>
+                    <h5 className="card-title">2021.2</h5>
+                      <p className="card-text">30/09/2021 à 07/10/2021</p>
+                      <p className="card-text"><small class="text-muted">Matrícula para o período 2021.2</small></p>
+                      <p className="card-text">18/10/2021 à 20/10/2021</p>
+                      <p className="card-text"><small class="text-muted">Re-matrícula para o período 2021.2</small></p>
+                      <p className="card-text">04/10/2021</p>
+                      <p className="card-text"><small class="text-muted">Início do período letivo 2021.2</small></p>
+                      <p className="card-text">25/10/2021</p>
+                      <p className="card-text"><small class="text-muted">Início do período trancamento de turma 2021.2</small></p>
+                      <p className="card-text">26/11/2021</p>
+                      <p className="card-text"><small class="text-muted">Término do período trancamento de turma 2021.2</small></p>
+                      <p className="card-text">19/02/2022</p>
+                      <p className="card-text"><small class="text-muted">Término do período letivo 2021.2</small></p>
                 </Card.Body>
             </Card>
           </section>    
